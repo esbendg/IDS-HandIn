f60d96519b73923b907e289f223c9af5dea28a3c
Xizoz <jonatan.meyer@hotmail.com>
2021-10-28 19:29:27 +0200

    Skiftet token

--- SpotifyCurrentSong.py
+++ SpotifyCurrentSong.py
@@ -6,7 +6,7 @@ from pprint import pprint
 pygame.init()
 
 SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
-SPOTIFY_ACCESS_TOKEN = 'BQAws2YkcesaalSkOIJp38MzIr8oLQ7lzQub74p0UxziZmQA9v7ULiiQXCJJlfqAsv8_SL6Ta9cwMCmIuMg6Pl4sWBPH4xXUd8V3j4qgm0g3Urbx3NwmirfDk488i7KX1R1Mq-_U4PRF_O7zGk0'
+SPOTIFY_ACCESS_TOKEN = 'BQAF_IlsDexOUueLNJzIhAVmF8VBtXx-0EAL9V5iCVcoFNTq-78s2NMxmBrKT5G8ckZZ6ZBm0l_tuNOULeX3Oz_ErfQaKYPhMP6eIruOJa3Yz92b1tjXn3cjHnbIb961huerCmfSnWhzmlTRaUM'
 
 
 def get_current_track(access_token):
@@ -46,11 +46,10 @@ def music():
 		    	indent=4,
 		    )
 		    current_track_id = current_track_info['id']
-
+    
 	    time.sleep(1)
     
 
 if __name__ == '__main__':
     music()
-
 
