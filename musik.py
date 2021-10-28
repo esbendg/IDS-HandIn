6fe163c82ffd73b82c25043283b0bd29586315fd
Xizoz <jonatan.meyer@hotmail.com>
2021-10-28 17:45:24 +0200

    Comments clarifying the musik.py file

--- musik.py
+++ musik.py
@@ -1,32 +1,32 @@
 from abc import get_cache_token
 import pygame
-import requests
 import spotipy
-import json
 from spotipy.client import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from pprint import pprint
 import sys
 
+#Client id, and secret client id from the Spotify Developer Dashboard
 cid = '2979387358d646089e17a9f58f435b71'
 secret = '38abc9ffc9a84a21b38c3928646685a3'
 
+#Client and Secret Client id being validaded
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp= spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-# Nedenstående viser forskellige sange fra en kunstner.
+# 20 tracks from an artist being printet to the terminal
 results = sp.search(q='aj tracy', limit=20)
 for i, t in enumerate(results['tracks']['items']):
     print(' ', i, t['name'])
 
 
-# Forskellige informationer om en kunstner
+# Various types of infomation about a given artist
 if len(sys.argv) > 1:
-    urn = sys.argv[1]
+    kunstner = sys.argv[1]
 else:
-    urn = 'spotify:artist:1d5Y2zrhRQ6R0plv652L67'
+    kunstner = 'spotify:artist:1d5Y2zrhRQ6R0plv652L67'
 
-artist = sp.artist(urn)
+artist = sp.artist(kunstner)
 pprint(artist)
 
