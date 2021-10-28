db6d5a57059c9ac7d94b6549ddecb20dae2f2386
Xizoz <jonatan.meyer@hotmail.com>
2021-10-28 18:33:42 +0200

    Tkinter added

--- musik.py
+++ musik.py
@@ -5,6 +5,7 @@ from spotipy.client import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from pprint import pprint
 import sys
+import tkinter as tk
 
 # Client id, and secret client id from the Spotify Developer Dashboard
 cid = '2979387358d646089e17a9f58f435b71'
@@ -13,6 +14,7 @@ secret = '38abc9ffc9a84a21b38c3928646685a3'
 # Client and Secret Client id being validaded
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 
 # 20 tracks from an artist being printet to the terminal
@@ -30,3 +32,10 @@ else:
 artist = sp.artist(kunstner)
 pprint(artist)
 
+# Shows the infomation from above in a window
+window = tk.Tk()
+window.wm_attributes('-transparentcolor', '#ab23ff')
+infomationer = tk.Label(text=artist, fg="white", bg="#ab23ff" ,width=100, height=30, wraplength=200, font="helvetica 9")
+infomationer.pack()
+
+window.mainloop()
